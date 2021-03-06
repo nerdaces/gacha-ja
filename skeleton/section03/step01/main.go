a3ace7b3d0bf47941fd3048ac85e9dd0da096415
nerdaces <yuyammiki@gmail.com>
2021-03-06 12:44:12 +0900

    solved section03

--- skeleton/section03/step01/main.go
+++ skeleton/section03/step01/main.go
@@ -13,7 +13,12 @@ func main() {
 	// 現在時刻をUNIX時間にしたものを種とする
 	rand.Seed(time.Now().Unix())
 
-	// TODO: 以下の処理をdraw関数として定義する
+	// TODO: draw関数を呼ぶ
+	draw()
+}
+
+// TODO: 以下の処理をdraw関数として定義する
+func draw() {
 
 	// 0から99までの間で乱数を生成する
 	num := rand.Intn(100)
@@ -33,6 +38,4 @@ func main() {
 	}
 
 	//  -- ここまで
-
-	// TODO: draw関数を呼ぶ
 }
